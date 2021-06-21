96dd8a6b9280340395e5508b769f41089df926c6
Chirpentine Jones <chirpentine@gmail.com>
2021-06-21 12:54:53 -0500

    Support for vuelta/url base parameter in download script

--- get_actas.py
+++ get_actas.py
@@ -9,6 +9,7 @@ import lib
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', help='Only download actas para envío al JEE', action='store_true')
 parser.add_argument('-n', help='Download specific acta(s)')
+parser.add_argument('-v', help='Download specific vuelta (v1, v2, or url base)', default='v2')
 args = parser.parse_args()
 
 logging.basicConfig(level=logging.INFO)
@@ -27,7 +28,7 @@ if args.a or args.n is not None or i is not None:
         toGet = lib.ID_RANGE[lib.ID_RANGE.index(i):]
     errors = 0
 
-    c = lib.ONPEClient()
+    c = lib.ONPEClient(args.v)
 
     try:
         for i in toGet:
