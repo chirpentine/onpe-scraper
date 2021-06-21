a11c1b1ad461422e1d23b9761ccdc55d5215482c
Chirpentine Jones <chirpentine@gmail.com>
2021-06-21 15:48:40 -0500

    Update to support generation of actas.csv file from primera vuelta data

--- squeeze.py
+++ squeeze.py
@@ -1,45 +1,60 @@
+import os
 import csv
+import argparse
 
 import lib
 
-fieldnames = [ 'mesa',
-               'dpto',
-               'provincia',
-               'distrito',
-               'local',
-               'direcc',
-               'electores.hab',
-               'votantes',
-               'estado.acta',
-               'votos.validos',
-               'votos.blancos',
-               'votos.nulos',
-               'votos.impug',
-               'votos.fp',
-               'votos.pl',
-               'votos.emit' ]
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', help='Data dir, default "data"', default='data')
+args = parser.parse_args()
+
+partyMap = { 'TOTAL VOTOS VALIDOS':                            'validos',
+             'VOTOS EN BLANCO':                                'blancos',
+             'VOTOS NULOS':                                    'nulos',
+             'VOTOS IMPUGNADOS':                               'impug',
+             'TOTAL VOTOS EMITIDOS':                           'emit',
+
+             'FUERZA POPULAR':                                 'fp',
+             'PARTIDO POLITICO NACIONAL PERU LIBRE':           'pl',
+             'PARTIDO NACIONALISTA PERUANO':                   'pnp',
+             'EL FRENTE AMPLIO POR JUSTICIA, VIDA Y LIBERTAD': 'fa',
+             'PARTIDO MORADO':                                 'pm',
+             'PERU PATRIA SEGURA':                             'pps',
+             'VICTORIA NACIONAL':                              'vn',
+             'ACCION POPULAR':                                 'ap',
+             'AVANZA PAIS - PARTIDO DE INTEGRACION SOCIAL':    'avp',
+             'PODEMOS PERU':                                   'pp',
+             'JUNTOS POR EL PERU':                             'jpp',
+             'PARTIDO POPULAR CRISTIANO - PPC':                'ppc',
+             'UNION POR EL PERU':                              'upp',
+             'RENOVACION POPULAR':                             'rp',
+             'RENACIMIENTO UNIDO NACIONAL':                    'run',
+             'PARTIDO DEMOCRATICO SOMOS PERU':                 'sp',
+             'DEMOCRACIA DIRECTA':                             'pdd',
+             'ALIANZA PARA EL PROGRESO':                       'app' }
+
+rows = []
+for fn in os.listdir(args.v):
+    d = lib.readActa(os.path.join(args.v, fn))
+    aut = {}
+    for e in d['votos']:
+        aut[e['AUTORIDAD']] = e['congresal']
+    row = { 'mesa':          lib.iForFn(fn),
+            'dpto':          d['presidencial']['DEPARTAMENTO'],
+            'provincia':     d['presidencial']['PROVINCIA'],
+            'distrito':      d['presidencial']['DISTRITO'],
+            'local':         d['presidencial']['TNOMB_LOCAL'],
+            'direcc':        d['presidencial']['TDIRE_LOCAL'],
+            'electores.hab': d['presidencial']['NNUME_HABILM'],
+            'votantes':      d['presidencial']['TOT_CIUDADANOS_VOTARON'],
+            'estado.acta':   d['presidencial']['OBSERVACION_TXT'] }
+    for p, v in aut.items():
+        if p in partyMap:
+            row['votos.%s' % partyMap[p]] = v
+    rows.append(row)
 
 with open('actas.csv', 'w') as csvfile:
-    csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    csvwriter = csv.DictWriter(csvfile, fieldnames=list(rows[0].keys()))
     csvwriter.writeheader()
-    for i in lib.ID_RANGE:
-        d = lib.readActa(lib.fnForI(i))
-        aut = {}
-        for e in d['votos']:
-            aut[e['AUTORIDAD']] = e['congresal']
-        csvwriter.writerow({ 'mesa':          i,
-                             'dpto':          d['presidencial']['DEPARTAMENTO'],
-                             'provincia':     d['presidencial']['PROVINCIA'],
-                             'distrito':      d['presidencial']['DISTRITO'],
-                             'local':         d['presidencial']['TNOMB_LOCAL'],
-                             'direcc':        d['presidencial']['TDIRE_LOCAL'],
-                             'electores.hab': d['presidencial']['NNUME_HABILM'],
-                             'votantes':      d['presidencial']['TOT_CIUDADANOS_VOTARON'],
-                             'estado.acta':   d['presidencial']['OBSERVACION_TXT'],
-                             'votos.validos': aut['TOTAL VOTOS VALIDOS'],
-                             'votos.blancos': aut['VOTOS EN BLANCO'],
-                             'votos.nulos':   aut['VOTOS NULOS'],
-                             'votos.impug':   aut['VOTOS IMPUGNADOS'],
-                             'votos.fp':      aut['FUERZA POPULAR'],
-                             'votos.pl':      aut['PARTIDO POLITICO NACIONAL PERU LIBRE'],
-                             'votos.emit':    aut['TOTAL VOTOS EMITIDOS'] })
+    for row in rows:
+        csvwriter.writerow(row)
