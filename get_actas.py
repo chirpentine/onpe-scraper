37f8cb50150404979a1c0e1e1bae971531d361a2
Chirpentine Jones <chirpentine@gmail.com>
2021-06-22 15:05:50 -0500

    Remove info made redundant by argparse output

--- get_actas.py
+++ get_actas.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-a', help='Only download actas para envío al JEE', action='store_true')
 parser.add_argument('-n', help='Download specific acta(s)')
 parser.add_argument('-v', help='Download specific vuelta (v1, v2, or url base)', default='v2')
-parser.add_argument('-b', help='Backend to use (ONPEClient or ONPEClientParallel)', default='ONPEClient', choices=[ 'ONPEClient', 'ONPEClientParallel' ])
+parser.add_argument('-b', help='Backend to use', default='ONPEClient', choices=[ 'ONPEClient', 'ONPEClientParallel' ])
 args = parser.parse_args()
 
 i = lib.nextI()
