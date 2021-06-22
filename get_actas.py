6549c09c54cd5772486136ac734aed03df9e0f4f
Chirpentine Jones <chirpentine@gmail.com>
2021-06-22 13:42:54 -0500

    Refactor and add parallel client

--- get_actas.py
+++ get_actas.py
@@ -1,18 +1,14 @@
-import time
-import json
-import pickle
-import logging
 import argparse
 
 import lib
+from lib import logging
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', help='Only download actas para envío al JEE', action='store_true')
 parser.add_argument('-n', help='Download specific acta(s)')
 parser.add_argument('-v', help='Download specific vuelta (v1, v2, or url base)', default='v2')
+parser.add_argument('-b', help='Backend to use (ONPEClient or ONPEClientParallel)', default='ONPEClient', choices=[ 'ONPEClient', 'ONPEClientParallel' ])
 args = parser.parse_args()
-
-logging.basicConfig(level=logging.INFO)
 
 i = lib.nextI()
 if args.a or args.n is not None or i is not None:
@@ -28,27 +24,10 @@ if args.a or args.n is not None or i is not None:
         toGet = lib.ID_RANGE[lib.ID_RANGE.index(i):]
     errors = 0
 
-    c = lib.ONPEClient(args.v)
+    c = eval('lib.' + args.b)(args.v, args.a)
 
     try:
-        for i in toGet:
-            try:
-                ad = c.getActa(i)
-                if 'generalPre' in ad['procesos']:
-                    # we got data
-                    errors = 0
-                    if (args.a and 'JEE' not in ad['procesos']['generalPre']['presidencial']['OBSERVACION_TXT']) or (not args.a):
-                        fn = lib.fnForI(i)
-                        pickle.dump(ad, open(fn, 'wb'))
-                        logging.info('Wrote %s', fn)
-            except json.decoder.JSONDecodeError:
-                errors += 1
-                if errors == 5:
-                    logging.error('Exiting due to excess errors')
-                    break
-                else:
-                    logging.error('JSONDecodeError, sleeping 1s')
-                    time.sleep(1)
+        c.getList(toGet)
     finally:
         del c
 
--- lib.py
+++ lib.py
@@ -1,8 +1,13 @@
 import os
 import json
+import time
 import pickle
+import logging
+import threading
 
 from selenium import webdriver
+
+logging.basicConfig(level=logging.INFO)
 
 DATA_DIR = 'data'
 ID_RANGE = list(range(1, 83381)) + list(range(900001, 903109))
@@ -39,12 +44,17 @@ def readActa(fn):
 def actasJEE():
     return [ i for i in haveI() if 'JEE' in readActa(fnForI(i))['presidencial']['OBSERVACION_TXT'] ]
 
+def chunks(l, n):
+    for i in range(n):
+        yield l[i::n]
+
 class ONPEClient:
-    def __init__(self, baseUrl='v2'):
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference('', False)
-        self.d = webdriver.Firefox(firefox_profile=fp)
+    def __init__(self, baseUrl='v2', onlyJEE=False):
         self.setBaseUrl(baseUrl)
+        self.onlyJEE = onlyJEE
+        self.d = None
+
+        logging.debug('ONPEClient(%s) initted', id(self))
 
     def getActa(self, i):
         self.d.get(self.baseUrl + ('%s?name=param' % fmtId(i)))
@@ -55,5 +65,52 @@ class ONPEClient:
                    'v2': 'https://api.resultadossep.eleccionesgenerales2021.pe/mesas/detalle/' }
         self.baseUrl = baseUrl if baseUrl not in urlmap else urlmap[baseUrl]
 
+    def setupWebdriver(self):
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('', False)
+        self.d = webdriver.Firefox(firefox_profile=fp)
+
+    def getList(self, l):
+        logging.debug('ONPEClient(%s).getList(%s)', id(self), l[:5])
+        if self.d is None:
+            self.setupWebdriver()
+
+        logging.debug('ONPEClient(%s).getList(%s) webdriver setup complete, going to run', id(self), l[:5])
+        errors = 0
+        for i in l:
+            try:
+                ad = self.getActa(i)
+                if 'generalPre' in ad['procesos']:
+                    # we got data
+                    errors = 0
+                    if (self.onlyJEE and 'JEE' not in ad['procesos']['generalPre']['presidencial']['OBSERVACION_TXT']) or (not self.onlyJEE):
+                        fn = fnForI(i)
+                        pickle.dump(ad, open(fn, 'wb'))
+                        logging.info('Wrote %s', fn)
+            except json.decoder.JSONDecodeError:
+                errors += 1
+                if errors == 5:
+                    logging.error('Exiting due to excess errors')
+                    break
+                else:
+                    logging.error('JSONDecodeError, sleeping 1s')
+                    time.sleep(1)
+
     def __del__(self):
-        self.d.quit()
+        if self.d is not None:
+            self.d.quit()
+
+
+class ONPEClientParallel:
+    def __init__(self, *args, **kwargs):
+        self.args   = args
+        self.kwargs = kwargs
+
+    def getList(self, l, workers=2):
+        wp = []
+        wt = []
+        for ll in chunks(l, workers):
+            wp.append(ONPEClient(*self.args, **self.kwargs))
+            wt.append(threading.Thread(target=wp[-1].getList, args=(ll,)))
+            wt[-1].start()
+        [ t.join() for t in wt ]
